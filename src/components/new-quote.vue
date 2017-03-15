dbcc264ef5b9c1670b91652c18f9593c03d6a6be
Junaid KHALID <junaidkhalid1@aol.com>
2017-03-15 23:22:33 +0100

    * New quote vue file implementation

---
+++ src/components/new-quote.vue
@@ -0,0 +1,28 @@
+<template>
+    <form @submit.prevent="onSubmitted">
+        <div class="form-group">
+            <label for="content">Content</label>
+            <input
+                    type="text"
+                    id="content"
+                    class="form-control"
+                    v-model="quoteContent">
+        </div>
+        <button class="btn btn-primary" type="submit">Submit</button>
+    </form>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                quoteContent: ""
+            }
+        },
+        methods: {
+            onSubmitted() {
+
+            }
+        }
+    }
+</script>
